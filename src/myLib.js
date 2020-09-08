55aaf20a5fcb1ae970364c235ffe511aab093c3f
abc <32093132+langehk@users.noreply.github.com>
2020-09-08 10:54:20 +0200

    maxNumber

--- src/myLib.js
+++ src/myLib.js
@@ -1,13 +1,8 @@
-import mylib2  from './myLib2.js';
-
-let input1 = document.getElementById("input1").value;
-let input2 = document.getElementById("input2").value;
 
 
-
-
-//Calculate prime number
 const isPrime = function (arg) {
+    
+    requireMaxSize(arg);
     for (var i = 2; i < arg; i += 1) {
         if (arg % i === 0) {
             return false;
--- src/myLib2.js
+++ src/myLib2.js
@@ -1,5 +1,19 @@
 
+let maxNumber = 9007199254740991;
 
-function smartfunk(){
+function requireMaxSize(maxInt){
+
+    if(maxInt < 0)
+    {
+        alert("Enter a higher number");
+    }
+    else if(maxInt >= maxNumber)
+    {
+       alert("Your number is too high!");
+    }
+
+    return maxInt;
 
 }
+
+
