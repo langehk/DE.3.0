c3374e18532fbbc2a9005b075d7af5f58037deb6
abc <32093132+langehk@users.noreply.github.com>
2020-09-08 11:53:35 +0200

    Update myLib.js

--- src/myLib.js
+++ src/myLib.js
@@ -1,12 +1,19 @@
 
-
 const isPrime = function (arg) {
-    
+    console.time("timer");
+
     requireMaxSize(arg);
-    for (var i = 2; i < arg; i += 1) {
+    for (var i = (arg-1); i >= 2; i -= 1) {
+      debugger;
         if (arg % i === 0) {
+            
+            console.timeEnd("timer");
+            console.log("false");
             return false;
         }
     }
+    console.timeEnd("timer");
+    console.log("true");
     return true;
+    
 }
