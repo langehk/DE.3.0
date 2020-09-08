00585277c205b829d34682b902c7a8c88ab5c562
abc <32093132+langehk@users.noreply.github.com>
2020-09-08 12:07:05 +0200

    v3

--- src/myLib.js
+++ src/myLib.js
@@ -4,13 +4,26 @@ const isPrime = function (arg) {
 
     requireMaxSize(arg);
     for (var i = (arg-1); i >= 2; i -= 1) {
-      debugger;
-        if (arg % i === 0) {
+
+      if(arg == 2)
+      {
+        console.timeEnd("timer");
+        console.log("true");
+        return true;    
+      }
+      else if (arg % 2 === 0) {
+            
+        console.timeEnd("timer");
+        console.log("false");
+        return false;
+      }
+        else if (arg % i === 0) {
             
             console.timeEnd("timer");
             console.log("false");
             return false;
         }
+        
     }
     console.timeEnd("timer");
     console.log("true");
